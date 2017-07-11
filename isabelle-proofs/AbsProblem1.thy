2d2cb2e79f1bb39fc9eb4e14ceb06ab6097e8958
Cristina Matache <cris.matache@gmail.com>
2017-07-11 12:24:06 +0100

    finish abs-problem-1

--- isabelle-proofs/AbsProblem1.thy
+++ isabelle-proofs/AbsProblem1.thy
@@ -113,7 +113,8 @@ proof(rule ccontr)
        
    (*resolve*)
    have refute_0_18: "-1 < x"
-     using refute_0_15 by sos
+     using refute_0_15
+     by (sos "((R<1 + (((A<=1 * R<1) * (R<1 * [1]^2)) + ((A<=0 * R<1) * (R<1 * [1]^2)))))")
    
    (*resolve*)
    have refute_0_19: "ln(1 + x) < x"
@@ -185,7 +186,116 @@ proof(rule ccontr)
      using refute_0_28
      by (metis add.commute add_uminus_conv_diff le_less_trans ln_lower_1 ln_lower_1_eq not_le)
      
+   (*resolve*)
+   have refute_0_30: "(-1 + (1 + x)) / (1 + x) < x * (1 + x * -1) \<or>
+                      1 + x \<le> 0 \<or>
+                      x \<le> ln(1 + x)"
+     using refute_0_29 refute_0_24 by auto
+       
+   (*arithmetic*)
+   have refute_0_31: "x / (1 + x) < x * (1 + x * -1) \<or>
+                      x \<le> -1 \<or>
+                      x \<le> ln(1 + x)"
+     using refute_0_30 by auto
+       
+   (*resolve*)
+   have refute_0_32: "x / (1 + x) < x * (1 + x * -1) \<or>
+                      x \<le> ln(1 + x)"
+     using refute_0_31 refute_0_18 by auto
+       
+   (*resolve*)
+   have refute_0_33: "x / (1 + x) < x * (1 + x * -1)"
+     using refute_0_32 refute_0_19 by auto
+       
+   (*resolve*)
+   have refute_0_34: "x < x * (1 + x * -1) * (1 + x) \<or> 
+                      1 + x \<le> 0"
+     using refute_0_0 refute_0_33 by auto
+       
+   (*arithmetic*)
+   have refute_0_35: "0 < x * (x * (x * -1)) \<or> 
+                      x \<le> -1"
+     using refute_0_34
+       proof -
+         have f1: "ln (x + 1) < x"
+           by (simp add: add.commute refute_0_19)
+         have f2: "x * x < \<bar>- x + ln (x + 1)\<bar>"
+           by (metis add.commute mult_minus1_right refute_0_2)
+         have "0 \<le> ln (x + 1)"
+        using refute_0_15 by force
+      then have "x * x < x"
+        using f2 f1 by linarith
+      then show ?thesis
+        using abs_ln_one_plus_x_minus_x_bound_nonneg less_eq_real_def negate_0_0 by auto
+    qed
+
+   (*subst*)
+    have refute_0_36: "x < x * (1 + x) \<or> 
+                      1 + x \<le> 0 \<or>
+                      x \<le> x / (1 + x)"
+      using leq_right_divide_mul_pos_axiom
+      using not_le by blast
+        
+   (*subst*)
+    have refute_0_37: "(-1 + (1 + x)) / (1 + x) < x \<or> 
+                       1 + x \<le> 0 \<or>
+                       x \<le> ln(1 + x)"
+      using refute_0_28
+        proof -
+          have "1 / (1 + x) < 1 \<longrightarrow> x / (1 + x) < x \<or> (\<exists>r. \<not> x * r < x \<and> r < 1)"
+            by (metis (no_types) mult.right_neutral times_divide_eq_right)
+          then have "x / (1 + x) < x \<or> (- 1 + (1 + x)) / (1 + x) < x \<or> 1 + x \<le> 0 \<or> x \<le> ln (1 + x)"
+            using refute_0_15 by force
+          then show ?thesis
+        by simp
+    qed
+      
+   (*resolve*)
+   have refute_0_38: "(-1 + (1 + x)) / (1 + x) < x \<or> 
+                       1 + x \<le> 0"   
+      using refute_0_37 refute_0_19 by auto
+        
+   (*arithmetic*)
+   have refute_0_39: "x / (1 + x) < x \<or> 
+                      x \<le> -1"
+     using refute_0_38 by auto
+       
+   (*resolve*)
+   have refute_0_40: "x / (1 + x) < x"
+     using refute_0_39 refute_0_18 by auto
+       
+   (*resolve*)
+   have refute_0_41: "x < x * (1 + x) \<or> 
+                      1 + x \<le> 0"
+     using refute_0_36 refute_0_40 by auto
+       
+   (*arithmetic*) 
+   have refute_0_42: "0 < x * x \<or> x \<le> -1"
+     using refute_0_41
+     using less_eq_real_def by auto
+   
+   (*decision*)   
+   have refute_0_43: "0 < x * x \<or> -1 < x"
+     using refute_0_15
+     by (sos "((R<1 + (((A<=1 * R<1) * (R<1 * [1]^2)) + ((A<=0 * R<1) * (R<1 * [1]^2)))))")
+      
+   (*resolve*)
+   have refute_0_44: "0 < x * x"
+     using refute_0_42 refute_0_43 by auto
+       
+   (*decision*)
+   have refute_0_45: "x * (x * (x * -1)) \<le> 0 \<or> 
+                      x \<le> -1"
+     using refute_0_44 refute_0_15
+     by (sos "((((A<0 * (A<1 * A<2)) * R<1) + ((A<=0 * (A<1 * R<1)) * (R<1 * [x^2]^2))))")
+     
+   (*resolve*)
+   have refute_0_46: "x \<le> -1"
+     using refute_0_45 refute_0_35 by auto
+   
+   (*resolve*)    
    then show False
+     using refute_0_45 refute_0_18 by auto
 qed      
   
 end
