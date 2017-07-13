cd22d60bf3ee824e79d969c84ebd37d9c3e130de
Cristina Matache <cris.matache@gmail.com>
2017-07-13 11:21:28 +0100

    try to parse tstp file

---
+++ proof_reconstruction/Isar.thy
@@ -0,0 +1,12 @@
+theory Isar
+  imports Main
+begin
+  
+ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_parsing.ML"
+
+ML\<open>
+hash_string "abc"; 
+\<close>
+  
+end
+  
