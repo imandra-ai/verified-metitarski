def33715b3004eb93fa5364db717443f52422dda
Cristina Matache <cris.matache@gmail.com>
2017-07-14 17:07:45 +0100

    small redirection example

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -8,7 +8,7 @@ ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_pa
 
 ML\<open>
 ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3", "4", [])])] 
-    (TSTP_Input.read "Documents/internship/verified-metitarski/problems/cristina-problem-5.tstpout") 
+    (TSTP_Input.read "Documents/internship/verified-metitarski/problems/cristina-problem-6.tstpout") 
 \<close>
   
 ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close>  
