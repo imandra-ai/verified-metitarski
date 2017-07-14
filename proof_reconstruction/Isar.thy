f993d52214baf3b94fa02724e38516aeee383460
Cristina Matache <cris.matache@gmail.com>
2017-07-14 11:08:05 +0100

    parse a tstp proof

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -3,11 +3,14 @@ theory Isar
 begin
 
 ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_input.ML"  
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_parsing.ML"
+(*ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_parsing.ML"*)
 
 ML\<open>
-hash_string "abc"; 
+ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3", "4", [])])] 
+    (TSTP_Input.read "Documents/internship/verified-metitarski/problems/cristina-problem-5.tstpout") 
 \<close>
+  
+ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close>  
   
 end
   
