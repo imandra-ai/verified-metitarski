f4045318656529948ff88dd802e9a8bafa02d1d9
Cristina Matache <cris.matache@gmail.com>
2017-07-17 12:25:12 +0100

    input tstp proof without header. not working in Isabelle/ML

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -10,6 +10,8 @@ ML\<open>
 ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3", "4", [])])] 
     (TSTP_Input.read "Documents/internship/verified-metitarski/problems/cristina-problem-6.tstpout") 
 \<close>
+
+ML\<open>TSTP_Input.read "Documents/internship/verified-metitarski/proof_reconstruction/input.txt"\<close>
   
 ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close>  
   
