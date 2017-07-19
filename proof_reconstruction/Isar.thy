7d91dee7dae60294cc5103fe44ce8b7b72fb826a
Cristina Matache <cris.matache@gmail.com>
2017-07-19 09:40:38 +0100

    try to convert a clause into an isabelle term

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -14,6 +14,7 @@ ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3",
 ML\<open>TSTP_Input.read "Documents/internship/verified-metitarski/proof_reconstruction/input.txt"\<close>
   
 ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close>  
+    
   
 end
   
