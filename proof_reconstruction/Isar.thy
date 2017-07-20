7e527e2b6835f1894391e38361265a213a6423a2
Cristina Matache <cris.matache@gmail.com>
2017-07-20 11:02:55 +0100

    synthesise a proof for cristina-problem-6

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -14,11 +14,8 @@ ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3",
 ML\<open>TSTP_Input.read "Documents/internship/verified-metitarski/proof_reconstruction/input.txt"\<close>
   
 ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close> 
-ML\<open>open Symtab\<close>  
-ML\<open>open Term
-   val f = add_vars;
-\<close>  
-    
+  
+
   
 end
   
