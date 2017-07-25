d092213862ae660b41fb88c9ae91be5f5ca8ad02
Cristina Matache <cris.matache@gmail.com>
2017-07-25 17:00:14 +0100

    replace name of constants with iabelle name

--- proof_reconstruction/Isar.thy
+++ proof_reconstruction/Isar.thy
@@ -4,19 +4,14 @@ begin
 
 declare[[ML_print_depth=50]]  
   
+ML {* @{term "1"} *}  
+ML\<open>Syntax.read_term @{context} "1"\<close>  
+  
 ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_input.ML" 
 
 ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/tstp_parsing.ML"
-
-ML\<open>
-ATP_Satallax.atp_proof_of_tstplike_proof "1" [("2", [ATP_Problem.Class_Decl("3", "4", [])])] 
-    (TSTP_Input.read "Documents/internship/verified-metitarski/problems/cristina-problem-6.tstpout") 
-\<close>
-
-ML\<open>TSTP_Input.read "Documents/internship/verified-metitarski/proof_reconstruction/input.txt"\<close>
   
-ML\<open>Sledgehammer_Prover_ATP.run_atp   Sledgehammer_Prover.Try   ""\<close> 
-ML\<open>open Symtab\<close>  
+ML\<open>open Symtab\<close>
 
   
 end
