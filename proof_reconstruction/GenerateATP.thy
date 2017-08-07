4901239fbed7405dc12fb800b881fd49406cd72c
Cristina Matache <cris.matache@gmail.com>
2017-08-07 12:24:02 +0100

    add type information to atp_terms

--- proof_reconstruction/GenerateATP.thy
+++ proof_reconstruction/GenerateATP.thy
@@ -4,7 +4,7 @@ begin
   
 declare[[ML_print_depth=50]]   
   
-lemma foo: "\<forall>X Y .\<exists> Z. \<not> Z = (1::real) \<and> less_eq 0 (times (minus X 1::real) (minus X 1))"  
+lemma foo: "\<forall>X Y::real .\<exists> Z. \<not> Z = (1::real) \<and> less_eq 0 (times (minus X 1::real) (minus X 1))"  
   sorry
    
 
@@ -32,8 +32,18 @@ ML\<open>
 val theorem = Thm.concl_of @{thm foo};
 \<close>  
   
-
+  
 ML\<open>
+fun atp_type_of_type ctxt ty =
+  (case ty of
+    Type ("fun", [t1, t2]) => ATP_Problem.AFun (atp_type_of_type ctxt t1, atp_type_of_type ctxt t2)
+  | Type (name, ls) => ATP_Problem.AType ((name, []), map (atp_type_of_type ctxt) ls)
+  | TFree (name, _) => ATP_Problem.AType ((name, []), [])
+
+  (*Not dealing with schematic type variables*)
+  | _ => error ("Malformed type: " ^ (Syntax.string_of_typ ctxt ty)) 
+  )
+
 (*Fix types*)
 fun atp_term_of_term ctxt args t =
   (case (t, args) of
@@ -43,10 +53,10 @@ fun atp_term_of_term ctxt args t =
   | (Abs (s, _, t'), []) => ATP_Problem.AAbs (((s, ATP_Problem.AType (("real", []), [])), atp_term_of_term ctxt [] t'), [])
   | (Abs _, _) => error "Term not beta-normalized"
 *)
-  | (Const (name, _), ls) => ATP_Problem.ATerm ((name, []), map (atp_term_of_term ctxt []) ls)
+  | (Const (name, ty), ls) => ATP_Problem.ATerm ((name, [atp_type_of_type ctxt ty]), map (atp_term_of_term ctxt []) ls)
 
-(*Fix bound variables*)
-  | (Bound i, ls) => ATP_Problem.ATerm (("bound."^(string_of_int i), []), map (atp_term_of_term ctxt []) ls)
+(*Bound variables can only be real*)
+  | (Bound i, []) => ATP_Problem.ATerm (("bound."^(string_of_int i), [ATP_Problem.AType (("Real.real", []), [])]), [])
 
 (*Not dealing with free and schematic variables Free and Var*)
   | _ => error (Syntax.string_of_term ctxt t ^ "(" ^ commas (map (Syntax.string_of_term ctxt) args)  ^ ")")
@@ -69,12 +79,12 @@ fun atp_formula_of_term ctxt t =
         (Const ("HOL.Trueprop", @{typ "bool\<Rightarrow>prop"})) => atp_formula_of_term ctxt t2
       | (Const ("HOL.All", _)) =>
           (case t2 of 
-            (Abs (var, _, t3)) =>  ATP_Problem.AQuant (ATP_Problem.AForall, [(var, SOME (ATP_Problem.AType (("real", []), [])))], atp_formula_of_term ctxt t3)
+            (Abs (var, _, t3)) =>  ATP_Problem.AQuant (ATP_Problem.AForall, [(var, SOME (ATP_Problem.AType (("Real.real", []), [])))], atp_formula_of_term ctxt t3)
           |  _ => error ("Error in:" ^ Syntax.string_of_term ctxt t)
           )
       | (Const ("HOL.Ex", _)) =>
           (case t2 of 
-            (Abs (var, _, t3)) =>  ATP_Problem.AQuant (ATP_Problem.AExists, [(var, SOME (ATP_Problem.AType (("real", []), [])))], atp_formula_of_term ctxt t3)
+            (Abs (var, _, t3)) =>  ATP_Problem.AQuant (ATP_Problem.AExists, [(var, SOME (ATP_Problem.AType (("Real.real", []), [])))], atp_formula_of_term ctxt t3)
           |  _ => error ("Error in:" ^ Syntax.string_of_term ctxt t)
           )
 
@@ -126,7 +136,7 @@ fun fix_bound_vars bound_vars atp_formula =
   end;
 
 fix_bound_vars [] (atp_formula_of_term @{context} (Envir.beta_norm theorem));
-
+                                                                       
 \<close>
     
 end
