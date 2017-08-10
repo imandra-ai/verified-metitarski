77d392bfe93246c7d32f1eecc60e8f6ed7fcb048
Cristina Matache <cris.matache@gmail.com>
2017-08-10 16:53:24 +0100

    add ml function that calls metitarski

--- proof_reconstruction/GenerateATP.thy
+++ proof_reconstruction/GenerateATP.thy
@@ -5,7 +5,7 @@ begin
 declare[[ML_print_depth=50]]   
   
 (*For the cube root use "root 3"*)  
-lemma foo: "\<forall>X Y::real .\<forall> Z::real. Z \<noteq> (tan 5.3) \<longrightarrow> greater pi (times ( minus (abs X) 1::real) (minus X 1))"  
+lemma foo: "\<forall>X::real.(0 \<le> X \<longrightarrow> abs(ln(1+X) - X) <= X^2) "  
 (*apply(atomize)*)
   sorry
  
@@ -15,15 +15,20 @@ val theorem = Thm.concl_of @{thm foo};
 
 ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/atp_problem_of_thm.ML"  
 ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/atp_problem_to_tptp.ML"
-
+ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/mt_call.ML"
   
 ML\<open>
 val atp_problem = ATP_Problem_of_thm.atp_problem_of_thm @{context} @{thm foo};                                                                    
 \<close>
   
 ML\<open>
-ATP_Problem_to_tptp.atp_problem_to_tptp atp_problem;
-\<close> 
+val tptp_problem = ATP_Problem_to_tptp.atp_problem_to_tptp atp_problem;
+\<close>
+
+ML\<open>
+val tptp_proof = Call_Metitarski.call_mt tptp_problem
+\<close>  
+
     
 end
   
