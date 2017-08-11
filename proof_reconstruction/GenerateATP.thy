98c0353948b45124b25cdc99c6f2afda5e0d054b
Cristina Matache <cris.matache@gmail.com>
2017-08-11 12:32:50 +0100

    translate atp formulae to terms

--- proof_reconstruction/GenerateATP.thy
+++ proof_reconstruction/GenerateATP.thy
@@ -46,15 +46,70 @@ val trimmed_tptp_proof : string =
 
 val atp_proof = ATP_Satallax.atp_proof_of_tstplike_proof "1" atp_problem trimmed_tptp_proof
 
-(*Remove the clause derived by strip*)
+(*Remove the clause derived by strip=subgoal_0*)
 fun remove_strip accum [] = rev accum
    |remove_strip accum ((step as (_, _, _, rule, _))::proof) =   
         if rule = "strip" then remove_strip accum proof
         else remove_strip (step::accum) proof
 
-val atp_proof = remove_strip [] atp_proof
+(*Clauses derived from subgoal_0 now derived from original goal*)
+fun replace_from accum _ [] = rev accum
+   |replace_from accum name'' ((name, role, t, rule, from)::proof) = 
+        let val from' = map (fn (name',ls) => if name' = "subgoal_0" then (name'', ls) else (name',ls)) from
+        in replace_from ((name, role, t, rule, from')::accum) name'' proof
+        end
+
+val conjecture_name = (#1 (#1 (hd atp_proof)))
+                handle Empty => raise Fail "The parsed atp_proof is empty." 
+
+val atp_proof = 
+    atp_proof
+        |>remove_strip []
+        |>replace_from [] conjecture_name
 \<close>
-  
+
+ML\<open>
+fun atp_formula_to_term (atp_formula 
+  : (string, string ATP_Problem.atp_type, 
+     (string, string ATP_Problem.atp_type) ATP_Problem.atp_term, string) ATP_Problem.atp_formula) =
+  (case atp_formula of
+    ATP_Problem.AQuant (quantifier, [(var, _)], phi) =>
+      let
+        val quant_string =
+          (case quantifier of
+            ATP_Problem.AForall => "HOL.All"
+          | ATP_Problem.AExists => "HOL.Ex"
+          | _ => error "Not a valid atp_formula"
+          )
+      in
+        (Const (quant_string, @{typ  "(real \<Rightarrow> bool) \<Rightarrow> bool"}) $
+         Abs (var, @{typ "real"}, (atp_formula_to_term phi)))
+      end
+
+  | ATP_Problem.AConn (conn, [phi1, phi2]) =>
+      let
+        val conn_string =
+          (case conn of
+            ATP_Problem.AImplies => "HOL.implies"
+          | ATP_Problem.AAnd => "HOL.conj"
+          | ATP_Problem.AOr => "HOL.disj"
+          | _ => error "Unupported atp connective"
+          )
+      in
+        (Const (conn_string, @{typ "bool \<Rightarrow> bool \<Rightarrow> bool"})) $ 
+        (atp_formula_to_term phi1) $
+        (atp_formula_to_term phi2)
+      end
+
+  | ATP_Problem.AConn (ATP_Problem.ANot, [phi]) =>
+      (Const ("HOL.Not", @{typ "bool \<Rightarrow> bool"})) $
+      (atp_formula_to_term phi)
+
+  | ATP_Problem.AAtom atp_term => error "" (*atp_term_to_term atp_term*)
+
+  | _ => error "Unsupported atp_formula."
+  )
+\<close>  
   
 end
   
