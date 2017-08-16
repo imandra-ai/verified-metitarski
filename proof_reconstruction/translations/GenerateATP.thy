362b3be90a911b18714a5f448e0fc4439013c094
Cristina Matache <cris.matache@gmail.com>
2017-08-16 10:06:30 +0100

    remove absolute paths from GenerateATP

--- proof_reconstruction/translations/GenerateATP.thy
+++ proof_reconstruction/translations/GenerateATP.thy
@@ -14,13 +14,15 @@ lemma foo: "\<forall>(Y::real).0 <= abs (Y^3) "
 ML\<open>
 val theorem = Thm.concl_of @{thm foo};
 \<close>  
- 
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/config.ML"  
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/thm_to_atp_problem.ML"  
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/atp_problem_to_tptp.ML"
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/mt_call.ML"
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/tptp_proof_to_atp_proof.ML"
-ML_file "~/Documents/internship/verified-metitarski/proof_reconstruction/translations/termify_atp_proof.ML"  
+
+(*No absolute paths needed because this theory is in the same folder as the ML files.*)  
+ML_file "config.ML"  
+
+ML_file "thm_to_atp_problem.ML"  
+ML_file "atp_problem_to_tptp.ML"
+ML_file "mt_call.ML"
+ML_file "tptp_proof_to_atp_proof.ML"
+ML_file "termify_atp_proof.ML"  
   
   
 (*Create ATP_Problem from a theorem*)  
@@ -44,7 +46,8 @@ ML\<open>
 val atp_proof = TPTP_Proof_to_atp_proof.tptp_proof_to_atp_proof atp_problem tptp_proof
 (*Need to deal with the case MT gives up*)
 \<close>
-  
+
+(*Termify the formulas in the atp_proof*)  
 ML\<open>
 val termified_atp_proof : (term, string) ATP_Proof.atp_step list 
     = Termify_atp_proof.termify_atp_proof atp_proof;
