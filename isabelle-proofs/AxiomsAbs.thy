309d75e2cb1ff0adebdee110741a8ba4b557783a
Cristina Matache <cris.matache@gmail.com>
2017-08-17 12:06:01 +0100

    sorry all steps in isar proofs

--- isabelle-proofs/AxiomsAbs.thy
+++ isabelle-proofs/AxiomsAbs.thy
@@ -2,11 +2,11 @@ theory AxiomsAbs
   imports Main Real
 begin
   
-lemma abs_nonnegative:
+lemma abs_nonnegative_axiom:
   "\<not>(0 \<le> (x::real)) \<or> (abs x) = x"
   by auto
     
-lemma abs_negative:
+lemma abs_negative_axiom:
   "0 \<le> (x::real) \<or> (abs x) = -x "
   by auto
   
