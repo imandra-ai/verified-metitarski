8e957a8e6c9e402a7f6f986de9a06409cd42b637
Cristina Matache <cris.matache@gmail.com>
2017-08-21 18:26:55 +0100

    investigate further the goal argument of isar_proof_text

--- proof_reconstruction/translations/GenerateATP.thy
+++ proof_reconstruction/translations/GenerateATP.thy
@@ -14,12 +14,29 @@ declare[[ML_print_depth=50]]
   
 (*Redirected proof involves a case split. Not supporting that at the moment.*)  
 (*lemma foo: "\<forall>(Y::real).0 <= abs(Y^3)"*)
-lemma foo: "\<forall>(X::real).(0\<le>X \<longrightarrow> abs(ln(1+X)-X) \<le> X^2)"
-(*lemma foo: "\<forall>(X::real) (Y::real).X+Y \<le> abs (X+Y)"*)
-  
-  sorry  
-   
+(*lemma foo: "\<forall>(X::real).(0\<le>X \<longrightarrow> abs(ln(1+X)-X) \<le> X^2)"*)
+lemma foo: "\<forall>(X::real) (Y::real).X+Y \<le> abs (X+Y)"  
+  ML_val {* 
+  val st = #goal @{Isar.goal};
+  (*Thm.prop_of (#goal @{Isar.goal})*) *}
+    apply(tactic {*fn st => (writeln (@{make_string} st); Seq.single st) *})
+  ML_prf{**}
+sorry    
     
+  
+ML\<open>
+  val conjecture : term = List.hd (Syntax.check_props @{context}
+    [Syntax.parse_prop @{context} "\<forall>(X::real) (Y::real).X+Y \<le> abs (X+Y)"])
+
+  val conjecture = Const ("Pure.imp", @{typ "prop \<Rightarrow> prop \<Rightarrow> prop"}) $
+                   conjecture $ 
+                   (Const ("Pure.prop", @{typ "prop \<Rightarrow> prop"}) $ conjecture)
+
+  (*val conjecture : cterm = Thm.cterm_of @{context} conjecture;
+
+  @{thm conjecture}*)
+\<close>  
+  
 ML\<open>
 val theorem = Thm.concl_of @{thm foo};
 fun delimiter #"." = true
@@ -47,7 +64,10 @@ fun write (file : string) (text : string) =
   
 (*No absolute paths needed because this theory is in the same folder as the ML files.*)  
 ML_file "config.ML"  
+ML_file "atp_util.ML" 
+ML_file "sledgehammer_isar.ML"
 
+  
 ML_file "thm_to_atp_problem.ML"  
 ML_file "atp_problem_to_tptp.ML"
 ML_file "mt_call.ML"
@@ -56,8 +76,7 @@ ML_file "termify_atp_proof.ML"
 
 (*ML_file "termified_atp_proof_to_isar.ML" *)
 ML_file "termified_atp_proof_to_indirect_proof.ML"  
-  
-ML_file "sledgehammer_isar.ML"
+
 
   
 (*Create ATP_Problem from a term*)  
@@ -109,21 +128,25 @@ val num_chained : int = 1 (*What is this?*)
   val try0 : bool = true
   val minimize : bool = false
   val atp_proof0 : (term, string) ATP_Proof.atp_step list = termified_atp_proof
-  val goal : thm = @{thm foo}
+  val goal : term (*thm*) = conjecture (*@{thm foo}*)
 val isar_params : unit ->Sledgehammer_Isar.isar_params = 
   fn () => (verbose, alt_metis_args, preplay_timeout, compress, try0, minimize, atp_proof0, goal)
 
   val used_facts : (string * Sledgehammer_Isar.stature) list = []
   val preplay : Sledgehammer_Proof_Methods.proof_method = Sledgehammer_Proof_Methods.Auto_Method  (*?*)
-  val one_line_play : Sledgehammer_Proof_Methods.play_outcome = Sledgehammer_Proof_Methods.Play_Failed  (*?*)
+  val one_line_play : Sledgehammer_Proof_Methods.play_outcome = Sledgehammer_Proof_Methods.Played Time.zeroTime  (*?*)
   val banner : string = ""
   val subgoal : int = 1
   val subgoal_count : int = 1
 val one_line_params : Sledgehammer_Isar.one_line_params =
   ((used_facts, (preplay, one_line_play)), banner, subgoal, subgoal_count);
 
-Sledgehammer_Isar.isar_proof_text ctxt debug num_chained isar_proofs smt_proofs 
-  isar_params one_line_params
+ML_Pretty.format_polyml 86 (ML_Pretty.to_polyml (ML_Pretty.str (Sledgehammer_Isar.isar_proof_text ctxt debug num_chained isar_proofs smt_proofs 
+  isar_params one_line_params)))
+\<close>  
+ 
+ML\<open>
+open ML_Pretty
 \<close>  
   
 ML\<open>
