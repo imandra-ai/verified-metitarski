b77230703879d3f1f1a1910615cce109ab9beba6
Cristina Matache <cris.matache@gmail.com>
2017-08-29 17:53:36 +0100

    add algebra_simps to the simplifier for arith rules

--- proof_reconstruction/translations/Example.thy
+++ proof_reconstruction/translations/Example.thy
@@ -2,7 +2,7 @@ theory Example
   imports Main Real Transcendental
 begin
   
-lemma refute_28_lemm: "(- 1 + X_000050) / X_000050 < X_000051 \<or> X_000050 \<le> 0 \<or> X_000051 \<le> ln X_000050"
+(*lemma refute_28_lemm: "(- 1 + X_000050) / X_000050 < X_000051 \<or> X_000050 \<le> 0 \<or> X_000051 \<le> ln X_000050"
   sorry    
   
 lemma "abs-problem-1": "\<forall>(X::real).(0\<le>X \<longrightarrow> abs(ln(1+X)-X) \<le> X^2)"
@@ -13,11 +13,28 @@ proof -
    have refute_30_neg: "\<not> (- 1 + (1 + rr)) / (1 + rr) < rr * (1 + rr * - 1) \<and> \<not> 1 + rr \<le> 0 \<and> \<not> rr \<le> ln (1 + rr)"    
      sorry
    then have refute_24_neg: "\<not> ln (1 + rr) < rr * (1 + rr * - 1) \<and> \<not> rr \<le> ln (1 + rr)"
-     using refute_28 by fastforce
+     using refute_28_lemm by fastforce
  }
    then show ?thesis
      sorry
-   qed    
+ qed
+*)   
+lemma "\<forall>(X::real).(0\<le>X \<longrightarrow> abs(ln(1+X)-X) \<le> X^2)"
+proof -
+  {
+    fix rr :: real
+    assume "\<not> 0 < rr * (rr * (rr * - 1)) \<and> \<not> rr \<le> - 1"
+    then have "\<not> rr < rr * (1 + rr * - 1) * (1 + rr) \<and> \<not> 1 + rr \<le> 0"
+      sledgehammer [max_facts = 25, isar_proof] (add: algebra_simps)
+      apply(simp add: algebra_simps)
+      sledgehammer[max_facts = 25]
+      by (smt mult.commute mult.left_commute semiring_normalization_rules(2))
+(*      by (smt mult.commute mult.left_commute semiring_normalization_rules(2)) ()*)
+(*      by (smt mult.assoc mult.commute mult_minus_right semiring_normalization_rules(2)) *)
+  }
+  then show ?thesis
+     sorry
+ qed
 end
   
   
